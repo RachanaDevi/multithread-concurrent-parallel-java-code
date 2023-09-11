2a00f8a2ce867ba7ec71ffa66b35b5843d862d63
Rachana Devi <rachanadevi18@gmail.com>
2023-09-11 23:31:47 +0530

    Rachana | Set priority and name for the thread

--- src/main/java/exercise1/Main.java
+++ src/main/java/exercise1/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Runnable runnable = () -> {
-            System.out.println("We are now in thread: "+ Thread.currentThread().getName());
+            System.out.println("We are now in thread: " + Thread.currentThread().getName());
+            System.out.println("Priority is: " + Thread.currentThread().getPriority());
         };
 
         Thread thread = new Thread(runnable);
-        System.out.println("Before starting thread, we are in thread: "+Thread.currentThread().getName());
-        thread.start();;
-        System.out.println("After starting thread, we are in thread: "+Thread.currentThread().getName());
+        thread.setName("New worker thread"); // adding names of thread with meaningful names
+        thread.setPriority(Thread.MAX_PRIORITY); // add priority
+        System.out.println("Before starting thread, we are in thread: " + Thread.currentThread().getName());
+        thread.start();
+        System.out.println("After starting thread, we are in thread: " + Thread.currentThread().getName());
     }
 }
