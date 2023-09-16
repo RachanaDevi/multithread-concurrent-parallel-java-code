39c62a19e777ea8b8011c58cb3a946b15330bf73
Rachana Devi <rachanadevi18@gmail.com>
2023-09-17 01:56:14 +0530

    Rachana | Add code for multi-threaded solution to decrease time for image processing

--- src/main/java/exercise7/ImageProcessing.java
+++ src/main/java/exercise7/ImageProcessing.java
@@ -4,22 +4,70 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageProcessing {
 
     private static final String SOURCE_FILE = "src/main/java/exercise7/resources/many-flowers.jpg";
     private static final String DESTINATION_FILE = "src/main/java/exercise7/out/many-flowers.jpg";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
         BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
 
+        /* for single thread it takes 648 milliseconds
+        long startTimeNonThread = System.currentTimeMillis();
+
         recolorSingleThreaded(originalImage, resultImage);
+
+        long endTimeNonThread = System.currentTimeMillis();
+        long durationNonThread = endTimeNonThread - startTimeNonThread;
+        System.out.println("Duration for single thread in milliseconds:" + durationNonThread);
+
+         */
+
+        // for multiple threads it takes 250 milliseconds - 10 thread
+        // for multiple threads it takes 373 milliseconds - 6 thread (6 core processor)
+        // for a single thread it takes 1085 seconds, because you have the overhead to start, join the thread etc
+        int numberOfThreads = 6;
+        long startTimeThread = System.currentTimeMillis();
+
+        recolorMultiThreaded(originalImage, resultImage, numberOfThreads);
+
+        long endTimeThread = System.currentTimeMillis();
+        long durationThread = endTimeThread - startTimeThread;
+        System.out.println("Duration for multi-threaded in milliseconds:" + durationThread);
+
         File outputFile = new File(DESTINATION_FILE);
         ImageIO.write(resultImage, "jpg", outputFile);
     }
 
-    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage){
+    public static void recolorMultiThreaded(BufferedImage originalImage, BufferedImage resultImage, int numberOfThreads) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight() / numberOfThreads;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            final int threadMultiplier = i;
+            Thread thread = new Thread(() -> {
+                int leftCorner = 0;
+                int topCorner = height * threadMultiplier;
+
+                recolorImage(originalImage, resultImage, leftCorner, topCorner, width, height);
+            });
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
+
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
         recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
     }
 
